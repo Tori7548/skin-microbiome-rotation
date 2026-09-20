a181ab5367058945e72fc5364865bd3781c087b4
Tori7548 <tteboh@constructor.university>
2026-09-20 10:29:39 +0200

    Update pipeline: clustering, Random Forest, importance results

--- hmp project.R
+++ hmp project.R
@@ -1,6 +1,6 @@
 install.packages("BiocManager")
 BiocManager::install("phyloseq")
-load(url("https://joey711.github.io/phyloseq-demo/HMPv35.RData"))
+##load(url("https://joey711.github.io/phyloseq-demo/HMPv35.RData"))
 BiocManager::install("HMP16SData")
 library(HMP16SData)
 V35_data <- V35()
@@ -64,4 +64,76 @@ ggplot(pca_df_log, aes(x = PC1, y = PC2, color = site)) +
   geom_point(alpha = 0.6) +
   labs(title = "PCA of Skin Microbiome Samples (log-transformed)", x = "PC1", y = "PC2") +
   theme_minimal()
+##saving all the plots because i forgot to save
+# 1. Raw PCA
+ggplot(pca_df, aes(x = PC1, y = PC2, color = site)) +
+  geom_point(alpha = 0.6) +
+  labs(title = "PCA of Skin Microbiome Samples", x = "PC1", y = "PC2") +
+  theme_minimal()
+ggsave("pca_raw_plot.png", width = 8, height = 6)
+# 2. Log-transformed PCA
+ggplot(pca_df_log, aes(x = PC1, y = PC2, color = site)) +
+  geom_point(alpha = 0.6) +
+  labs(title = "PCA of Skin Microbiome Samples (log-transformed)", x = "PC1", y = "PC2") +
+  theme_minimal()
+ggsave("pca_log_plot.png", width = 8, height = 6)
+# 3. Raw abundance histogram
+png("histogram_raw_abundance.png")
+hist(abundance_t, main = "Distribution of Raw Abundance Values", xlab = "Abundance")
+dev.off()
+# 4. Total reads per sample histogram
+png("histogram_sample_totals.png")
+hist(sample_totals, main = "Total Reads per Sample", xlab = "Total Abundance")
+dev.off()
+# 5. Bacteria prevalence histogram
+png("histogram_bacteria_prevalence.png")
+hist(bacteria_prevalence, main = "Number of Samples Each Bacteria Appears In", xlab = "Sample Count")
+dev.off()
+abundance_filtered <- abundance_t[, keep_cols]
+abundance_log_filtered <- log1p(abundance_filtered)
+dim(abundance_log_filtered)
+##asking for 2 clusters
+set.seed(42)
+km_result <- kmeans(abundance_log_filtered, centers = 2)
+table(km_result$cluster)
+##cross tabulate
+table(km_result$cluster, metadata_skin$site_simple)
+(475 + 291) / 990
+install.packages("mclust")
+library(mclust)
+adjustedRandIndex(km_result$cluster, metadata_skin$site_simple)
+##which bacteria drives each cluster
+# average abundance of each bacteria within each cluster
+cluster_means <- aggregate(abundance_log_filtered, by = list(cluster = km_result$cluster), FUN = mean)
+dim(cluster_means)
+##top drivers
+# difference between cluster 1 and cluster 2 for each bacteria
+cluster_diff <- cluster_means[1, -1] - cluster_means[2, -1]
+sorted_diff <- sort(cluster_diff, decreasing = TRUE)
+
+# top 10 bacteria more abundant in cluster 1
+head(sorted_diff, 10)
+
+# top 10 bacteria more abundant in cluster 2
+tail(sorted_diff, 10)
+##converting to plain numeric vector first
+cluster_diff <- as.numeric(cluster_means[1, -1] - cluster_means[2, -1])
+names(cluster_diff) <- colnames(cluster_means)[-1]
+
+sorted_diff <- sort(cluster_diff, decreasing = TRUE)
+
+# top 10 bacteria more abundant in cluster 1
+head(sorted_diff, 10)
+
+# top 10 bacteria more abundant in cluster 2
+tail(sorted_diff, 10)
+
+library(SummarizedExperiment)
+taxonomy <- as.data.frame(rowData(V35_data))
+head(taxonomy)
+top_cluster1_otus <- names(head(sorted_diff, 10))
+top_cluster2_otus <- names(tail(sorted_diff, 10))
+
+taxonomy[top_cluster1_otus, c("GENUS", "FAMILY")]
+taxonomy[top_cluster2_otus, c("GENUS", "FAMILY")]
 
